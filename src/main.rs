2bbbaea44c74df1d85ee60255bfb65d538bafda5
Louis Pilfold <louis@lpil.uk>
2020-09-14 15:12:00 +0100

    Simple drawing

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,28 @@ fn main() {
     nannou::app(model).update(update).simple_window(view).run();
 }
 
-struct Model {}
-
-fn model(_app: &App) -> Model {
-    Model {}
+struct Model {
+    count: usize,
 }
 
-fn update(_app: &App, _model: &mut Model, _update: Update) {}
+fn model(_app: &App) -> Model {
+    Model { count: 0 }
+}
 
-fn view(_app: &App, _model: &Model, frame: Frame) {
-    frame.clear(PURPLE);
+fn update(_app: &App, model: &mut Model, _update: Update) {
+    model.count = (model.count + 1) % 120;
+}
+
+fn view(app: &App, model: &Model, frame: Frame) {
+    let draw = app.draw();
+
+    if model.count > 60 {
+        draw.background().color(PURPLE);
+    } else {
+        draw.background().color(BLUE);
+    }
+
+    draw.ellipse().color(WHITE).xy(app.mouse.position());
+
+    draw.to_frame(app, &frame).unwrap();
 }
