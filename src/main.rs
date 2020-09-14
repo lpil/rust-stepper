de7e6173019f05f73640965de5d1b823d80ad40c
Louis Pilfold <louis@lpil.uk>
2020-09-14 15:52:51 +0100

    Flail'

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,66 @@
+use bitvec::prelude::*;
 use nannou::prelude::*;
+
+const CELL_SIZE: f32 = 50.0;
+const GAP_SIZE: f32 = 10.0;
 
 fn main() {
     nannou::app(model).update(update).simple_window(view).run();
 }
 
+struct Row {
+    cells: BitVec,
+}
+
+impl Row {
+    pub fn new() -> Self {
+        Self {
+            cells: bitvec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        }
+    }
+
+    pub fn rects(&self) -> Vec<Rect> {
+        let mut rects = Vec::with_capacity(self.cells.len());
+        for i in 0..self.cells.len() {
+            rects.push(
+                Rect::from_w_h(CELL_SIZE, CELL_SIZE).shift_x(i as f32 * (CELL_SIZE + GAP_SIZE)),
+            )
+        }
+        rects
+    }
+}
+
 struct Model {
-    count: usize,
+    rows: Vec<Row>,
 }
 
 fn model(_app: &App) -> Model {
-    Model { count: 0 }
+    Model {
+        rows: vec![
+            Row::new(),
+            Row::new(),
+            Row::new(),
+            Row::new(),
+            Row::new(),
+            Row::new(),
+            Row::new(),
+            Row::new(),
+        ],
+    }
 }
 
-fn update(_app: &App, model: &mut Model, _update: Update) {
-    model.count = (model.count + 1) % 120;
-}
+fn update(_app: &App, _model: &mut Model, _update: Update) {}
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
-
-    if model.count > 60 {
-        draw.background().color(PURPLE);
-    } else {
-        draw.background().color(BLUE);
-    }
-
+    draw.background().color(BLACK);
     draw.ellipse().color(WHITE).xy(app.mouse.position());
+
+    for row in &model.rows {
+        for rect in row.rects() {
+            draw.rect().xy(rect.xy()).wh(rect.wh()).color(WHITE);
+        }
+    }
 
     draw.to_frame(app, &frame).unwrap();
 }
